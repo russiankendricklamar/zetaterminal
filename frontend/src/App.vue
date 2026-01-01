0893015202f52a5edbb37cc127e1f47b78046dad
Egor Galkin <lifeofwhitedesiigner@gmail.com>
2026-01-01 16:17:49 +0300

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="dashboard-container">
+    <DashboardLayout />
+  </div>
+</template>
+
+<script setup lang="ts">
+import DashboardLayout from '@/components/Layout/DashboardLayout.vue'
+</script>
